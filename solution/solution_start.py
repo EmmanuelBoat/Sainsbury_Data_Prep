5999ebd7f30d96b65627e566de335b488cb07273
Emmanuel Boateng Sifah <emmanuelsifah@yahoo.com>
2020-03-22 15:59:39 +0000

    modify run_transformations function: products_df was modified to include initial of category

--- solution/solution_start.py
+++ solution/solution_start.py
@@ -4,12 +4,12 @@ import pandas as pd
 from pathlib import Path
 
 
+# Read csv files
 def read_csv(csv_filepath: str):
     return pd.read_csv(csv_filepath, header=0)
 
-
+# Read json files
 def read_json_folder(json_filepath: str):
-    #transactions_files = glob.glob("{}*/*.json".format(json_folder))
     transaction_files = Path(json_filepath).glob('**/*.json')
 
     return pd.concat(pd.read_json(f, lines=True) for f in transaction_files)
@@ -17,9 +17,23 @@ def read_json_folder(json_filepath: str):
 
 def run_transformations(customers_filepath: str, products_filepath: str, transactions_filepath: str, output_location: str):
     customers_df = read_csv(customers_filepath)
+
+    # Modify products_df
     products_df = read_csv(products_filepath)
+    product_list = []
+    for row in products_df.iterrows():
+        value = row[1][0], row[1][1], row[1][2], row[1][2][0].upper()
+        product_list.append(value)
+
+    # Convert list to dataframe and rename columns
+    products_df = pd.DataFrame(product_list)
+    products_df.columns = ('product_id', 'product_description', 'product_category', 'product_cat')
+
+
     transactions_df = read_json_folder(transactions_filepath)
 
+
+    #print(products_df)
     return get_latest_transaction_date(transactions_df)
 
 
@@ -36,9 +50,9 @@ def to_canonical_date_str(date_to_transform):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Transations_dataset')
-    parser.add_argument('--customers_filepath', required=False, default="./input_data_generator/input_data/starter/customers.csv")
-    parser.add_argument('--products_filepath', required=False, default="./input_data_generator/input_data/starter/products.csv")
-    parser.add_argument('--transactions_filepath', required=False, default="./input_data_generator/input_data/starter/transactions")
+    parser.add_argument('--customers_filepath', required=False, default="/Users/emmanuelsifah/Desktop/Git_Projects/input_data_generator/input_data/starter/customers.csv")
+    parser.add_argument('--products_filepath', required=False, default="/Users/emmanuelsifah/Desktop/Git_Projects/input_data_generator/input_data/starter/products.csv")
+    parser.add_argument('--transactions_filepath', required=False, default="/Users/emmanuelsifah/Desktop/Git_Projects/input_data_generator/input_data/starter/transactions")
     parser.add_argument('--output_location', required=False, default="./output_data/outputs/")
     args = vars(parser.parse_args())
 
